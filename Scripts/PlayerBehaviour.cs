6b404608ccf2b82fd09b0e484dcb6c00dafbe007
Kai Xuan <s10267360@connect.np.edu.sg>
2025-06-15 01:39:31 +0800

    UwU

--- Scripts/PlayerBehaviour.cs
+++ Scripts/PlayerBehaviour.cs
@@ -17,7 +17,7 @@ public class PlayerBehaviour : MonoBehaviour
 
     void OnFire()
     {
-        Debug.Log("bro");
+        Debug.Log("[OnFire] Firing projectile");
         GameObject newProjectile = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation);
         Vector3 fireForce = spawnPoint.forward * FireStrength;
         newProjectile.GetComponent<Rigidbody>().AddForce(fireForce);
@@ -25,11 +25,15 @@ public class PlayerBehaviour : MonoBehaviour
 
     void Update()
     {
-        // Raycast checking
-        RaycastHit hitInfo;
+        // Debug raycast line
         Debug.DrawRay(spawnPoint.position, spawnPoint.forward * interactionDistance, Color.green);
+
+        // Raycast check
+        RaycastHit hitInfo;
         if (Physics.Raycast(spawnPoint.position, spawnPoint.forward, out hitInfo, interactionDistance))
         {
+            Debug.Log("[Raycast] Hit object: " + hitInfo.collider.gameObject.name);
+
             if (hitInfo.collider.gameObject.CompareTag("Collectible"))
             {
                 if (currentCoin != null && currentCoin != hitInfo.collider.GetComponent<CoinBehaviour>())
@@ -41,47 +45,46 @@ public class PlayerBehaviour : MonoBehaviour
                 currentCoin = hitInfo.collider.gameObject.GetComponent<CoinBehaviour>();
                 currentCoin.Highlight();
 
-                Debug.Log("bro did u hit");
+                Debug.Log("[Raycast] Coin detected and highlighted");
             }
         }
         else if (currentCoin != null)
         {
+            Debug.Log("[Raycast] No collectible in view, unhighlighting");
             currentCoin.UnHighlight();
             currentCoin = null;
         }
 
-        // Interaction with coin
+        // Interaction input
         if (canInteract && Input.GetKeyDown(KeyCode.E))
         {
-            if (currentCoin != null)
-            {
-                currentCoin.Collect(this);
-                canInteract = false;
-            }
+            Debug.Log("[Update] 'E' pressed");
+            OnInteract();
         }
     }
 
     public void ModifyScore(int amount)
     {
         score += amount;
-        Debug.Log("Score: " + score);
+        Debug.Log("[Score] Modified. New score: " + score);
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("[CollisionEnter] Collided with: " + collision.gameObject.name);
+
         if (collision.gameObject.CompareTag("Collectible"))
         {
             ++score;
-            Debug.Log("Score: " + score);
+            Debug.Log("[CollisionEnter] Collected item. Score: " + score);
         }
         else if (collision.gameObject.CompareTag("Hazard"))
         {
             currentHealth -= 35;
-            Debug.Log("Health: " + currentHealth);
+            Debug.Log("[CollisionEnter] Hit hazard. Health: " + currentHealth);
             if (currentHealth < 1)
             {
-                Debug.Log("Player is dead");
-                // Add death logic here
+                Debug.Log("[CollisionEnter] Player is dead");
             }
         }
     }
@@ -93,54 +96,72 @@ public class PlayerBehaviour : MonoBehaviour
             if (currentHealth < maxHealth)
             {
                 currentHealth++;
-                if (currentHealth > maxHealth)
-                    currentHealth = maxHealth;
-
-                Debug.Log("Health: " + currentHealth);
+                if (currentHealth > maxHealth) currentHealth = maxHealth;
+                Debug.Log("[Healing] Health restored. Current health: " + currentHealth);
             }
         }
     }
-        void OnTriggerEnter(Collider other)
+
+    void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject.name);
+        Debug.Log("[TriggerEnter] Entered: " + other.gameObject.name);
+
         if (other.CompareTag("Collectible"))
         {
             canInteract = true;
             currentCoin = other.GetComponent<CoinBehaviour>();
+            Debug.Log("[TriggerEnter] Coin in range: " + currentCoin?.name);
         }
         else if (other.CompareTag("Door"))
         {
             canInteract = true;
             currentDoor = other.GetComponent<DoorBehaviour>();
+            Debug.Log("[TriggerEnter] Door in range: " + currentDoor?.name);
         }
     }
 
     void OnTriggerExit(Collider other)
     {
+        Debug.Log("[TriggerExit] Exited: " + other.gameObject.name);
+
         if (other.CompareTag("Collectible"))
         {
             if (currentCoin != null && other.gameObject == currentCoin.gameObject)
             {
                 canInteract = false;
                 currentCoin = null;
-                Debug.Log("Moved away from coin");
+                Debug.Log("[TriggerExit] Left collectible range");
+            }
+        }
+        else if (other.CompareTag("Door"))
+        {
+            if (currentDoor != null && other.gameObject == currentDoor.gameObject)
+            {
+                canInteract = false;
+                currentDoor = null;
+                Debug.Log("[TriggerExit] Left door range");
             }
         }
     }
+
     void OnInteract()
     {
-        if (canInteract)
+        Debug.Log("[Interact] Attempting interaction");
+
+        if (currentCoin != null)
         {
-            if (currentCoin != null)
-            {
-                Debug.Log("Interacting with coin");
-                currentCoin.Collect(this);
-            }
-            else if (currentDoor != null)
-            {
-                Debug.Log("Interacting with door");
-                currentDoor.Interact();
-            }
+            Debug.Log("[Interact] Interacting with coin");
+            currentCoin.Collect(this);
+        }
+        else if (currentDoor != null)
+        {
+            Debug.Log("[Interact] Interacting with door");
+            currentDoor.Interact();
+        }
+        else
+        {
+            Debug.Log("[Interact] Nothing to interact with");
         }
     }
 }
+
